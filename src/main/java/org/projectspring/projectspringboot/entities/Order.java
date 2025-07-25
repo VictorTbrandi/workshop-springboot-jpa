eba312d617b750aa07001e3a0dfff6f0571d033a
Victor Brandi <victorterrenguibrand@gmail.com>
2025-07-25 20:21:07 -0300

    OrderStatus enum

--- src/main/java/org/projectspring/projectspringboot/entities/Order.java
+++ src/main/java/org/projectspring/projectspringboot/entities/Order.java
@@ -2,6 +2,7 @@ package org.projectspring.projectspringboot.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import org.projectspring.projectspringboot.entities.enums.OrderStatus;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -21,6 +22,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,9 +31,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,15 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     @Override
